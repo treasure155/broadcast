190e5574b5c35db91d0358eac5b7a3c60d2e467b
Treasure Idem <treasurebis@gmail.com>
2024-06-22 17:21:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showNextSlide() {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 5000);
+
+
+
